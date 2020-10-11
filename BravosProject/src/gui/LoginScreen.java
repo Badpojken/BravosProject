7da23e4312f85f5f8588b67f0536c71d5a279610
Lenon <razorraver1@hotmail.com>
2020-10-11 04:50:05 -0300

    Testando se a interface vai funcionar

--- BravosProject/src/gui/LoginScreen.java
+++ BravosProject/src/gui/LoginScreen.java
@@ -50,39 +50,38 @@ public class LoginScreen extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
 		panel.setBounds(0, 0, 217, 553);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("VAI TER UMA FOTO");
 		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setBounds(45, 91, 126, 16);
 		panel.add(lblNewLabel);
-		
+
 		JLabel lblAlgumTextoAq = new JLabel("Algum texto aq proval");
 		lblAlgumTextoAq.setForeground(Color.WHITE);
 		lblAlgumTextoAq.setBounds(42, 488, 140, 16);
 		panel.add(lblAlgumTextoAq);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Usu\u00E1rio:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(229, 150, 92, 28);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Senha:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(229, 278, 92, 28);
 		contentPane.add(lblNewLabel_2);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		textField_1.setBounds(229, 191, 227, 42);
 		contentPane.add(textField_1);
-		
+
 		JButton btnNewButton = new JButton("Entrar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -90,7 +89,7 @@ public class LoginScreen extends JFrame {
 		});
 		btnNewButton.setBounds(229, 434, 129, 42);
 		contentPane.add(btnNewButton);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(229, 319, 227, 42);
 		contentPane.add(passwordField);
